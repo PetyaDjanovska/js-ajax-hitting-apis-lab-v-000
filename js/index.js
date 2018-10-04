d37e1f486e9e0028a9a8e519488120e12009ee07
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-04 18:59:24 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // your code here
+function getRepositories(){
+	let name = document.getElementById("username").value;
+	let url = `https://api.github.com/users/${name}/repos`;
+	const req = new XMLHttpRequest();
+	req.addEventListener('load', showRepositories);
+	req.open('GET', url);
+	req.send();
+}
+
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+
+}
