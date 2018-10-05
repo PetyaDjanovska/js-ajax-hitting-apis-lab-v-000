03142b975cbba3c7f732289400adfea9abbeff2c
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 15:31:14 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -35,4 +35,15 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
