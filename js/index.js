d8f0d62da414507f94448c34d8de37d19bc217eb
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 18:53:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -29,9 +29,10 @@ function getCommits(el) {
   const repoName = el.dataset.repo;
   const username = el.dataset.username;
   const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
   req.open('GET', `https://api.github.com/repos/${username}/${repoName}/commits`);
   req.send();
-  req.addEventListener('load', displayCommits);
+
 }
 
 function displayCommits() {
