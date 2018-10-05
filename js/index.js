8cd7de9db3c1337ef29f305d4a62f37d5072c5f2
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 16:58:39 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -35,6 +35,7 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
+  console.log(commits);
   const commitsList = `<ul>${commits
     .map(
       commit =>
