688865f202a3e9ff3677ba13b62e470f14b23d37
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 15:47:23 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ function showRepositories() {
     .map(
       r =>
         '<li>' +
-        r.name +
+        r.full_name +
         ' - <a href=`https://api.github.com/${r.full_name}`>link to URL</a></li>'
     )
     .join('')}</ul>`;
