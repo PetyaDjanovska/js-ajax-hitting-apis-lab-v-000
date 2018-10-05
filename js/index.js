cd215f3f9870dba9ee2af57031f740736e602ae4
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 17:19:47 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ function showRepositories() {
     .map(
       r =>
         '<li>' +
-        `<a href='https://api.github.com/${r.full_name}'>${r.name}</a>` +
+        t.html_url +
         ' - <a href="#" data-repo="' +
         r.name +
         '" onclick="getCommits(this)">Get Commits</a></li>'
