19b94873cf2b3f89b550a017de35692584300602
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 18:53:31 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ function getRepositories(){
 	let username = document.getElementById("username").value;
 	let url = `https://api.github.com/users/${username}/repos`;
 	const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
 	req.open('GET', url);
 	req.send();
-  req.addEventListener('load', displayRepositories);
 }
 
 
