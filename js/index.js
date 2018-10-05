dd739cd8e753941d1c4adbad6a0bfd37d4104d01
Petya Djanovska <petya.djanovska@gmail.com>
2018-10-05 15:41:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function showRepositories() {
         r.name +
         ' - <a href="#" data-repo="' +
         r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+        '" onclick="getCommits(this)">link to URL</a></li>'
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
